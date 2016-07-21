e35afec343ff982509702f3cfc1712e4307cd4a9
Benjamin Saunders <ben.e.saunders@gmail.com>
2016-07-20 19:34:37 -0700

    Missing tr calls

--- src/RoomMenu.cpp
+++ src/RoomMenu.cpp
@@ -4,10 +4,10 @@
 
 RoomMenu::RoomMenu(matrix::Room &room, QWidget *parent) : QMenu(parent), room_(room) {
   auto file_dialog = new QFileDialog(this);
-  auto upload = addAction(QIcon::fromTheme("document-open"), "Upload &file...");
+  auto upload = addAction(QIcon::fromTheme("document-open"), tr("Upload &file..."));
   connect(upload, &QAction::triggered, file_dialog, &QDialog::open);
   connect(file_dialog, &QFileDialog::fileSelected, &room, &matrix::Room::send_file);
   addSeparator();
-  auto leave = addAction(QIcon::fromTheme("system-log-out"), "Leave");
+  auto leave = addAction(QIcon::fromTheme("system-log-out"), tr("Leave"));
   connect(leave, &QAction::triggered, &room_, &matrix::Room::leave);
 }
