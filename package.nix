f3d9dd9478370de211aa00baf152df41f0fdb8a6
Benjamin Saunders <ben.e.saunders@gmail.com>
2016-07-28 03:11:41 -0700

    Sync binary name with tentative project name

--- package.nix
+++ package.nix
@@ -1,13 +1,13 @@
 {stdenv, cmake, ninja, qtbase, qtsvg, lmdb, makeQtWrapper}:
 
 stdenv.mkDerivation rec {
-  name = "native-chat-${version}";
+  name = "nachat-${version}";
   version = "0.0";
 
   buildInputs = [ cmake ninja qtbase qtsvg lmdb ];
   nativeBuildInputs = [ makeQtWrapper ];
   preFixup = ''
-    wrapQtProgram $out/bin/native-chat
+    wrapQtProgram $out/bin/nachat
   '';
 
   src = builtins.filterSource
