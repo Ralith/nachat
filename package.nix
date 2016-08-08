0641ff8d4fae4809de3df12b7af18532cd785c2e
Benjamin Saunders <ben.e.saunders@gmail.com>
2016-08-08 02:02:47 -0700

    Fix nix build

--- package.nix
+++ package.nix
@@ -1,17 +1,17 @@
-{stdenv, cmake, ninja, qtbase, qtsvg, lmdb, makeQtWrapper}:
+{stdenv, cmake, ninja, qtbase, qtsvg, lmdb, git, makeQtWrapper}:
 
 stdenv.mkDerivation rec {
   name = "nachat-${version}";
   version = "0.0";
 
-  buildInputs = [ cmake ninja qtbase qtsvg lmdb ];
+  buildInputs = [ cmake ninja qtbase qtsvg lmdb git ];
   nativeBuildInputs = [ makeQtWrapper ];
   preFixup = ''
     wrapQtProgram $out/bin/nachat
   '';
 
   src = builtins.filterSource
-    (path: type: type != "directory" || (baseNameOf path != ".git" && baseNameOf path != "build"))
+    (path: type: type != "directory" || baseNameOf path != "build")
     ./.;
   enableParallelBuilding = true;
 }
