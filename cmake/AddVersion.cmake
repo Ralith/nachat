cc4f5f026fd9b35f061bc8aab44eef766a402af6
Benjamin Saunders <ben.e.saunders@gmail.com>
2016-08-08 19:32:19 -0700

    Fix out-of-source builds
    verison.sh would fail when the build directory was not inside the source
    directory.

--- cmake/AddVersion.cmake
+++ cmake/AddVersion.cmake
@@ -2,7 +2,7 @@ set(VERSION_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/version.sh")
 
 function(add_version SOURCE)
   add_custom_command(
-    COMMAND "${VERSION_SCRIPT}" > "${SOURCE}"
+    COMMAND "${VERSION_SCRIPT}" "${CMAKE_CURRENT_SOURCE_DIR}" > "${SOURCE}"
     COMMENT "Generating version file ${SOURCE}"
     OUTPUT "${SOURCE}" .PHONY
     VERBATIM
--- cmake/version.sh
+++ cmake/version.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env sh
 set -eu
 
-COMMIT=$(git rev-parse HEAD)
+usage() {
+    echo "$0 <source directory>"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || usage
+
+SOURCE_DIR=$1
+
+_git() {
+  git -C "$SOURCE_DIR" "$@"
+}
+
+COMMIT=$(_git rev-parse HEAD)
 COMMIT_STR="0x$(echo $COMMIT |cut -b 1-2)"
 for i in `seq 3 2 39`; do
     COMMIT_STR="$COMMIT_STR, 0x$(echo $COMMIT |cut -b $i-$((i+1)))"
 done
-TAG=$(git describe --abbrev=0 --tags --always)
+TAG=$(_git describe --abbrev=0 --tags --always)
 cat <<EOF
 #include "version.hpp"
 namespace version {
 const uint8_t commit[20] = {$COMMIT_STR};
 const char tag[] = "$TAG";
-const bool dirty = $(if git diff-index --quiet HEAD; then echo "false"; else echo "true"; fi);
-const uint32_t commits_since_tag = $(git rev-list --count $TAG..$COMMIT);
+const bool dirty = $(if _git diff-index --quiet HEAD; then echo "false"; else echo "true"; fi);
+const uint32_t commits_since_tag = $(_git rev-list --count $TAG..$COMMIT);
 }
 EOF
