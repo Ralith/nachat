180cede9d0aa35c42e9b718c11213d2dd86e00a7
Benjamin Saunders <ben.e.saunders@gmail.com>
2016-08-09 20:11:57 -0700

    Remove unneeded debug print

--- src/TimelineView.cpp
+++ src/TimelineView.cpp
@@ -371,8 +371,6 @@ void TimelineView::prune_backlog() {
     batches_.pop_front();
     backlog_growable_ = true;
   }
-  if(events_removed > 0)
-    qDebug() << room_.id() << "pruned" << events_removed << "events";
 }
 
 void TimelineView::pop_front_block() {
