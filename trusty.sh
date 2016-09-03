42c690be16a0c4b30d9ef412576f23851d0d7166
Benjamin Saunders <ben.e.saunders@gmail.com>
2016-09-03 16:56:47 -0700

    Travis build

---
+++ trusty.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:beineri/opt-qt561-trusty
+sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
+sudo apt-get update -qq
+sudo apt-get install -qq -y gcc-${GCC_VERSION} g++-${GCC_VERSION} liblmdb-dev qt56base cmake
