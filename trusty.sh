b9fd5fea116547355e9a3254a775438582fc6270
Benjamin Saunders <ben.e.saunders@gmail.com>
2017-01-21 17:15:55 -0800

    Fix trusty Qt PPA reference

--- trusty.sh
+++ trusty.sh
@@ -2,7 +2,7 @@
 set -xeuo pipefail
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository -y ppa:beineri/opt-qt561-trusty
+sudo add-apt-repository -y ppa:beineri/opt-qt562-trusty
 sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo apt-get update -qq
 sudo apt-get install -qq -y gcc-${GCC_VERSION} g++-${GCC_VERSION} liblmdb-dev qt56base cmake
